400dd814b9b672c8ec34e4174ed5ae7de563806d
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2023-01-24 15:48:05 -0300

    Adicionando idade atual

--- arquivoJS/script.js
+++ arquivoJS/script.js
@@ -7,3 +7,17 @@ function mudarTema() {
     main.classList.toggle('dark_main')
     footer.classList.toggle('dark_footer')
 }
+
+function mostrarIdadeAtual() {
+    const idade = document.getElementById('idade')
+    const anoAtual = new Date().getFullYear()
+    const mesAtual = new Date().getMonth() + 1
+    const diaAtual = new Date().getDate()
+    let idadeAtual = undefined
+
+    mesAtual > 6 || (mesAtual === 6 && diaAtual >= 18) ? idadeAtual = anoAtual - 2005 : idadeAtual = anoAtual - 2005 - 1
+
+    idade.innerHTML = `Idade: ${idadeAtual} anos;`
+}
+
+window.addEventListener('load', mostrarIdadeAtual)
